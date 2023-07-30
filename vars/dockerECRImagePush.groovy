d90c031dfb31060518dcd9e21e99acd098109b2b
Adithya Vardhan <artisantek.adithya@gmail.com>
2023-07-31 02:12:11 +0530

    Groovy Error Fix

--- vars/dockerECRImagePush.groovy
+++ vars/dockerECRImagePush.groovy
@@ -5,7 +5,7 @@ def call(String dockerRegistry, String dockerImageTag, String awsCredID, String 
             curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null
             
             dpkg-query -l unzip 1>/dev/null
-            if [ $? -ne 0 ]; then
+            if [ \$? -ne 0 ]; then
                 echo "Unzip is not installed. Installing unzip..."
                 sudo apt update > /dev/null
                 sudo apt -y install unzip > /dev/null
