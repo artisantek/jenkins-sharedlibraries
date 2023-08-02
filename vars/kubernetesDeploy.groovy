d4e515eac6d8be8566f3681e2a545c61bcbb8458
Adithya Vardhan <artisantek.adithya@gmail.com>
2023-08-02 14:21:53 +0530

    Error fix

--- vars/kubernetesDeploy.groovy
+++ vars/kubernetesDeploy.groovy
@@ -1,6 +1,6 @@
 def call (String dockerRegistry, String dockerImageTag, String kubernetesDeployment, String kubernetesContainer) {
     sh """
-        kubectl get deploy $kubernetesDeployment || true if [ $? -eq 0 ]; then
+        kubectl get deploy $kubernetesDeployment || true if [ \$? -eq 0 ]; then
             echo "Updating image of deployment $kubernetesDeployment"
             kubectl set image deploy $kubernetesDeployment $kubernetesContainer="$dockerRegistry:$dockerImageTag" --record
         else
