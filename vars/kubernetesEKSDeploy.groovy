f47c34771ab85e058aadc59827da7b1aa4955955
Adithya Vardhan <artisantek.adithya@gmail.com>
2023-08-03 14:44:34 +0530

    Added kubectl install

--- vars/kubernetesEKSDeploy.groovy
+++ vars/kubernetesEKSDeploy.groovy
@@ -17,6 +17,10 @@ def call (String dockerRegistry, String dockerImageTag, String kubernetesDeploym
             rm -rf awscliv2.zip aws
             echo "AWS CLI installed successfully."
         fi
+        if ! command -v kubectl > /dev/null; then
+            curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+            sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+        fi
     """
 
     withCredentials([usernamePassword(
