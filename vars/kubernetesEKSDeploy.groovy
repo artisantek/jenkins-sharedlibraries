342e4d3371f4512efa11ce86104fdb06fa196109
Adithya Vardhan <artisantek.adithya@gmail.com>
2023-08-03 14:46:00 +0530

    Error fix

--- vars/kubernetesEKSDeploy.groovy
+++ vars/kubernetesEKSDeploy.groovy
@@ -18,7 +18,7 @@ def call (String dockerRegistry, String dockerImageTag, String kubernetesDeploym
             echo "AWS CLI installed successfully."
         fi
         if ! command -v kubectl > /dev/null; then
-            curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+            curl -LO "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
             sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
         fi
     """
