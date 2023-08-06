894e538327d3583ddcf736bc58d2a95e27a53a0b
Artisan Tek <113725746+artisantek@users.noreply.github.com>
2023-08-06 11:37:05 +0530

    Update kubernetesHelmDeploy.groovy

--- vars/kubernetesHelmDeploy.groovy
+++ vars/kubernetesHelmDeploy.groovy
@@ -1,4 +1,4 @@
-def call (String dockerRegistry, String dockerImageTag, String kubernetesDeployment, String kubernetesContainer, String helmChartName) {
-    sh 'helm upgrade --install $helmChartName helm/ --set image.repository="$dockerRegistry:$dockerImageTag" --set deployment.name=$kubernetesDeployment --set image.containerName=$kubernetesContainer '
+def call (String dockerRegistry, String dockerImageTag, String helmChartName) {
+    sh 'helm upgrade --install $helmChartName helm/ --set image.repository="$dockerRegistry:$dockerImageTag" '
 }
 
