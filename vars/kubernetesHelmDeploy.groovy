c6ce48342b7873823afd485358922ef6a98572fa
Artisan Tek <113725746+artisantek@users.noreply.github.com>
2023-12-16 15:48:09 +0530

    Added Helm Install

--- vars/kubernetesHelmDeploy.groovy
+++ vars/kubernetesHelmDeploy.groovy
@@ -1,4 +1,15 @@
 def call (String dockerRegistry, String dockerImageTag, String helmChartName) {
+    sh """
+        if ! command -v helm > /dev/null; then
+            echo "Helm not found. Installing Helm..."
+            curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+            chmod 700 get_helm.sh
+            ./get_helm.sh
+            rm -f get_helm.sh
+            echo "Helm installed successfully."
+        fi
+    """
+    
     sh 'helm upgrade --install $helmChartName helm/ --set image.repository="$dockerRegistry:$dockerImageTag" '
 }
 
