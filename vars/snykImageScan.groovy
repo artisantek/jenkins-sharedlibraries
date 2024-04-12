0fcdae33dbedb9f7dd87bc32a3639fc60c0412ab
Adithya Vardhan <artisantek.adithya@gmail.com>
2024-04-12 17:06:08 +0530

    Added Snyk Scan

---
+++ vars/snykImageScan.groovy
@@ -0,0 +1,13 @@
+def call(String dockerRegistry, String dockerImageTag, String snykCred, String snykOrg) {
+    // Use withCredentials to inject the Snyk token into the environment
+    withCredentials([string(
+        credentialsId: snykTokenCredID,
+        variable: 'snykToken'
+    )]) {
+        // Execute Snyk scan within a shell script
+        sh """
+            export SNYK_TOKEN=${snykToken}
+            snyk container monitor ${dockerRegistry}:${dockerImageTag} --org=${snykOrg}
+        """
+    }
+}
