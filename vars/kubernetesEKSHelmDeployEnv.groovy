bab8f514184a98d76cdbb9e94c0e466a63e88206
adithyagarapati <adithyavardhan93@gmail.com>
2024-07-21 14:01:56 +0530

    Updated to Use AWS Creds

--- vars/kubernetesEKSHelmDeployEnv.groovy
+++ vars/kubernetesEKSHelmDeployEnv.groovy
@@ -13,16 +13,13 @@ def call(String dockerRegistry, String dockerImageTag, String helmChartName, Str
     """
 
     // Using credentials to configure AWS
-    withCredentials([usernamePassword(
-        credentialsId: awsCredID,
-        usernameVariable: "awsAccessKey",
-        passwordVariable: "awsSecretKey"
-    )]) {
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: awsCredID]]) {
         sh """
-            aws configure set aws_access_key_id ${awsAccessKey}
-            aws configure set aws_secret_access_key ${awsSecretKey}
-            aws configure set region ${awsRegion}
-            aws eks --region ${awsRegion} update-kubeconfig --name ${eksClusterName}
+            # Configure AWS CLI
+            aws configure set aws_access_key_id \$AWS_ACCESS_KEY_ID
+            aws configure set aws_secret_access_key \$AWS_SECRET_ACCESS_KEY
+            aws configure set region $awsRegion
+            aws eks --region $awsRegion update-kubeconfig --name $eksClusterName
             
             if ! command -v helm > /dev/null; then
                 echo "Helm not found. Installing Helm..."
