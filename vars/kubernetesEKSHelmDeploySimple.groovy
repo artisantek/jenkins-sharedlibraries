8a2c2bdbdeeb636079e10791354fcb371f3f2eab
Artisan Tek <113725746+artisantek@users.noreply.github.com>
2025-11-16 11:10:46 +0530

    Set KUBECONFIG environment variable for Helm deploy

--- vars/kubernetesEKSHelmDeploySimple.groovy
+++ vars/kubernetesEKSHelmDeploySimple.groovy
@@ -1,4 +1,5 @@
 def call(String helmChartName, String kubernetesNamespace = 'default') {
     // Deploy using Helm
+    export KUBECONFIG="/home/ubuntu/.kube/config"
     sh "helm upgrade --install ${helmChartName} helm/ --namespace ${kubernetesNamespace} --create-namespace"
 }
