56676cea9af21b12a42b25d340d6da56bd2161f0
Artisan Tek <113725746+artisantek@users.noreply.github.com>
2025-11-16 11:17:20 +0530

    Update kubernetesEKSHelmDeploySimple.groovy

--- vars/kubernetesEKSHelmDeploySimple.groovy
+++ vars/kubernetesEKSHelmDeploySimple.groovy
@@ -1,7 +1,6 @@
 def call(String helmChartName, String kubernetesNamespace = 'default') {
     // Deploy using Helm
     sh """
-        export KUBECONFIG="/home/ubuntu/.kube/config"
         helm upgrade --install ${helmChartName} helm/ --namespace ${kubernetesNamespace} --create-namespace
     """
 }
