051cd18cbb89c3189f2e5185b6c2c340fae8f556
Artisan Tek <113725746+artisantek@users.noreply.github.com>
2025-11-16 10:01:20 +0530

    Add Helm deployment function for Kubernetes

---
+++ vars/kubernetesEKSHelmDeploySimple.groovy
@@ -0,0 +1,4 @@
+def call(String helmChartName, String kubernetesNamespace = 'default') {
+    // Deploy using Helm
+    sh "helm upgrade --install ${helmChartName} helm/ --namespace ${kubernetesNamespace} --create-namespace"
+}
